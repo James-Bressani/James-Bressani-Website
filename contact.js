0ccfcf9b63f1a575e5bb1f97f5334497eccbb003
James Bressani <bressanijames@gmail.com>
2025-10-05 10:24:21 -0400

    email works

--- contact.js
+++ contact.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const emailDisplay = document.getElementById('email-display');
   if (emailDisplay) {
     emailDisplay.addEventListener('click', function() {
-      const encodedEmail = 'amFtZXNicuZXNzYW5pQGdtYWlsLmNvbQ==';
+      const encodedEmail = 'YnJlc3NhbmlqYW1lc0BnbWFpbC5jb20=';
       const decodedEmail = atob(encodedEmail);
       this.innerHTML = `<a href="mailto:${decodedEmail}">${decodedEmail}</a>`;
       this.style.cursor = 'default';
